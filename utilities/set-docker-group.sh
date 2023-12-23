29e541afe5cdb21e9505ad6e77b09e3c53ceb235
BhuvanChand WSL <WSL@perosnalmachine.com>
2023-12-23 15:53:32 +0530

    Added few changes

--- utilities/set-docker-group.sh
+++ utilities/set-docker-group.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
-# Create Docker group and add Jenkins user to it
-addgroup -g $DOCKER_GID dockergroup
-addgroup jenkins dockergroup
+# Check if a group with DOCKER_GID already exists
+if getent group $DOCKER_GID; then
+    # Group exists, get its name
+    DOCKER_GROUP=$(getent group $DOCKER_GID | cut -d: -f1)
+else
+    # Group doesn't exist, create a new one named dockergroup
+    DOCKER_GROUP=dockergroup
+    addgroup -g $DOCKER_GID $DOCKER_GROUP
+fi
+
+# Add Jenkins user to the Docker group
+addgroup jenkins $DOCKER_GROUP
